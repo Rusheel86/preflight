5f34e730586f3cff7e9ccf09cce9d21e81d9cb80
Rusheel Sharma <rusheelhere@gmail.com>
2026-03-15 18:29:36 +0530

    fix: bump version to 0.1.1
    Signed-off-by: Rusheel Sharma <rusheelhere@gmail.com>

--- preflight/__init__.py
+++ preflight/__init__.py
@@ -3,5 +3,5 @@
 from preflight.registry import CheckResult, Registry, Severity
 from preflight.runner import run_checks
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["Registry", "CheckResult", "Severity", "run_checks"]
